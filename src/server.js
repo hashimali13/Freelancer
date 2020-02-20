608418b361c863ba41cd2e1ef3d2b7f17149ac0b
BGEnglish3 <59619118+BGEnglish3@users.noreply.github.com>
2020-02-20 22:46:29 +0000

    updated server
    minor update to server

--- src/server.js
+++ src/server.js
@@ -1,33 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3001
-const db = require('./db')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3001;
+const db = require("./db");
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true,
+    extended: true
   })
-)
+);
 
-app.get('/', (request, response) => {
-    response.json({ info: 'Basic route.' })
-  })
+app.get("/", (request, response) => {
+  response.json({ info: "Basic route." });
+});
 
-
-app.get('/authuser',db.authUser)
-app.get('/userid/id', db.searchUser)
-app.get('/users', db.getUsers)
+app.get("/authuser", db.authUser);
+app.get("/userid/id", db.searchUser);
+app.get("/users", db.getUsers);
 app.get("/getproject", db.getProjects);
-app.post('/create', db.createUser)
-app.get('/searchproject', db.searchProjects)
-
+app.post("/create", db.createUser);
+app.get("/searchproject", db.searchProjects);
+app.get("/seepost", db.seePost);
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
