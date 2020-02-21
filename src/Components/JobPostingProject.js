6edfc155e755a01eae56ebf3b6f0e07c8d331f88
yeezysenpai <hashimwars@gmail.com>
2020-02-21 15:33:53 +0000

    Style changes to project posting

--- src/Components/JobPostingProject.js
+++ src/Components/JobPostingProject.js
@@ -3,6 +3,8 @@ import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import Button from '@material-ui/core/Button';
+
 import Paper from "@material-ui/core/Paper";
 
 function JobPostingProject(props) {
@@ -37,14 +39,16 @@ function JobPostingProject(props) {
               elevation={3}
               style={{ padding: "50pt", paddingTop: "15px", width: "50%" }}
             >
-              <h1>{jobposting.title}</h1>
+              <Typography> <h1>{jobposting.title}</h1>
               <h3>Posted on {new Date(jobposting.postdate).toDateString()}</h3>
               <p>{jobposting.content}</p>
               <p>
                 To reference this job, use this code:
                 <b>{jobposting.jobid}</b>
               </p>
-              <button onClick={goBackHandle}>Go Back</button>
+              </Typography>
+              
+              <Button variant="contained" color="primary" onClick={goBackHandle}>Go Back</Button>
             </Paper>
           </Grid>
         </div>
