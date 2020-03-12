31722bb5fef2d78114ab77a0c4b22addff8b0ddd
BGEnglish3 <59619118+BGEnglish3@users.noreply.github.com>
2020-03-12 19:29:34 +0000

    Updated profile
    Code is working, still some styling work to do

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import RecentProjects from "./RecentProjects";
+import Typography from "@material-ui/core/Typography";
 
 function Profile(props) {
   const [data, setData] = useState([]);
+  const [data2, setData2] = useState([]);
+
   console.log(props);
   const history = useHistory();
 
@@ -24,39 +30,88 @@ function Profile(props) {
         console.log("you just activated my trap card, go errorsaur")
       );
 
-      axios
+    axios
       .get("http://localhost:3001/getProfile/:id", {
         params: {
           uid: props.match.params.id
         }
       })
-      .then(res => setData(res.data), console.log("adsfsaf"))
+      .then(res => setData2(res.data), console.log("adsfsaf"))
       .catch(err =>
-        console.log("you just activated my trap card, go errorsaur")
+        console.log("you just activated my trap card, go errorsxaur")
       );
   }, []);
+
+  const useStyles = makeStyles(theme => ({
+    container: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center"
+    },
+    leftColumn: {
+      order: 0
+    },
+    rightColumn: {
+      order: 1,
+      flexGrow: 2
+    }
+  }));
+
+  const style = useStyles();
 
   const showData = props => {
     return data.map(profile => {
       console.log(profile);
       return (
-        <div>
-          <h1> Hello {profile.firstname} </h1>
+        <div className={style.container}>
+          <div className={style.leftColumn}>
+            <Paper
+              elevation={3}
+              style={{ padding: "50pt", paddingTop: "15px", width: "25%" }}
+            >
+              <Typography>
+                <div>
+                  <img
+                    src={profile.profilepicture}
+                    style={{ width: "40%" }}
+                  ></img>
+                  <h1>
+                    {profile.firstname} {""} {profile.lastname}{" "}
+                  </h1>
+                  <h2>{profile.industry}</h2>
+                  <h4>{profile.location}</h4>
+                  <br />
+                  <h3>About:</h3>
+                  <h3>{profile.description}</h3>
+                  <hr />
+                  <h5>Languages:</h5>
+                  <h5>{profile.languages}</h5>
+                </div>
+                <ul>{showSkills(props)}</ul>
+              </Typography>
+            </Paper>
+          </div>
+          <Paper className={style.rightColumn}>
+            <RecentProjects user={profile.username}></RecentProjects>
+          </Paper>
         </div>
       );
     });
   };
+
+  const showSkills = props => {
+    return data2.map(skills => {
+      console.log(skills);
+      return <li>{skills.skill}</li>;
+    });
+  };
+
   return (
     <div>
-      <h1>Helloworld1 </h1>
+      {showData(props)}
       <Button variant="contained" color="primary" onClick={goBackHandle}>
         Go Back
       </Button>
-      <p>
-        Not fetching the data for some reason, gives me: 'GET
-        http://localhost:3001/getprofile?uid=1 404 (Not Found)'
-      </p>
-      {showData(props)}
     </div>
   );
 }
