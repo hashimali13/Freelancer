707a02126af28ba522347ae05ffccb107d31391a
BGEnglish3 <59619118+BGEnglish3@users.noreply.github.com>
2020-03-12 19:38:58 +0000

    Updated db to involve makepost

--- src/db.js
+++ src/db.js
@@ -86,6 +86,28 @@ const getMessages = (request, response) => {
   );
 };
 
+const makePost = (request, response) => {
+  console.log(request.body.title);
+  let title = request.body.title;
+  let content = request.body.content;
+  let pd = request.body.pd;
+  let dd = request.body.dd;
+  let jobtype = request.body.jobtype;
+  let uid = request.body.uid;
+  pool.query(
+    "INSERT INTO jobposting (title, content, postdate, deadline, jobtype, uid) VALUES ($1, $2, $3, $4, $5, $6)",
+    [title, content, pd, dd, jobtype, uid],
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        response.status(500).send({ error: "posting error" });
+      } else {
+        response.status(201).json(results.row);
+      }
+    }
+  );
+};
+
 const createUser = (request, response) => {
   console.log(request.body.user);
   let user = request.body.user;
@@ -191,5 +213,6 @@ module.exports = {
   seePost,
   searchProjects,
   getProfile,
-  getMessages
+  getMessages,
+  makePost
 };
