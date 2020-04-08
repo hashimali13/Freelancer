4d91c054b379193bfcc5584a2626993d6afa6402
bailey <englishkid1998@gmail.com>
2020-04-08 15:25:53 +0100

    Search creation and update pull

---
+++ src/Components/Search.js
@@ -0,0 +1,12 @@
+// import React from "react"
+
+// function Search(props) {
+// const [data, setData] = useState([]);
+// const [data2, setData2] = useState([]);
+
+// useEffect(() => {
+//   axios.get("http://localhost:3001/users").then((res) => setData(res.data), console.log("call to get all users"))
+// })
+// }
+
+// export default Search
